90536c28b50642e27433d8fb3183a046a89bcd30
Roberto Gamboni <roberto.gamboni@gmail.com>
2014-03-10 12:22:35 +0100

    adding stubbed template folder

---
+++ template/Header file.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// #OWNER# ___PROJECTNAME___
+// Copyright (c) ___YEAR___ #OWNER#
+// All Rights Reserved Worldwide
+// Proprietary and Confidential - Not for Distribution
+// Written by #AUTHOR#
+//
+// ___FILENAME___
+//
+// AUTHOR IDENTITY:
+//		___FULLUSERNAME___		___DATE___
+//    
+////////////////////////////////////////////////////////////////////////////////
+#ifndef ___FILEEXTENSION_______FILEBASENAMEASIDENTIFIER___
+#define ___FILEEXTENSION_______FILEBASENAMEASIDENTIFIER___
+
+
+
+#endif //! defined(___FILEEXTENSION_______FILEBASENAMEASIDENTIFIER___)
