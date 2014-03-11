566ba1576884ca17970795e748484dacaf35f793
Roberto Gamboni <roberto.gamboni@gmail.com>
2014-03-11 10:27:53 +0100

    updated comments to match appledoc style and removed doxygen group syntax

--- template/source_template/Objective-C category.xctemplate/NSObject/___FILEBASENAME___.h
+++ template/source_template/Objective-C category.xctemplate/NSObject/___FILEBASENAME___.h
@@ -28,5 +28,3 @@ ___IMPORTHEADER_categoryClass___
 @interface ___VARIABLE_categoryClass:identifier___ (___VARIABLE_categoryName:identifier___)
 
 @end
-
-//@}
--- template/source_template/Objective-C class.xctemplate/NSObject/___FILEBASENAME___.h
+++ template/source_template/Objective-C class.xctemplate/NSObject/___FILEBASENAME___.h
@@ -29,4 +29,3 @@ ___IMPORTHEADER_foundationSubclass___
 
 @end
 
-//@}
